c6436ceaf292ac91f9d13aa91aa9c5ede68b6dcf
AchuAshwath <achuashwath88@gmail.com>
2025-05-06 15:24:42 +0530

    add and tried out jasmine for testing

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,24 @@
+import { formatMoney} from "../scripts/utils/money.js";
+
+// test suite
+describe("test suite :  formatMoney", () => {
+    // test case 1
+    it("test case 1: formatMoney function works correctly for 1000 cents", () => {
+        expect(formatMoney(1000)).toEqual("$10.00");
+    });
+    
+    // test case 2
+    it("test case 2: formatMoney function works correctly for 0 cents", () => {
+        expect(formatMoney(0)).toEqual("$0.00");
+    });
+    
+    // test case 3
+    it("test case 3: formatMoney function works correctly for 2000.5 cents", () => {
+        expect(formatMoney(2000.5)).toBe("$20.01");
+    });
+    
+    // test case 4
+    it("test case 4: formatMoney function works correctly for 2000.4 cents", () => {
+        expect(formatMoney(2000.4)).toBe("$20.00");
+    });
+});
--- test/moneyTest.js
+++ test/moneyTest.js
@@ -1,33 +1,33 @@
-import { formatmoney } from "../scripts/utils/money.js";
+import { formatMoney } from "../scripts/utils/money.js";
 
 
-// bsasic test cases for formatmoney function
-if(formatmoney(1000) === '$10.00') {
-    console.log("test passed: formatmoney function works correctly for 1000 cents.");
+// bsasic test cases for formatMoney function
+if(formatMoney(1000) === '$10.00') {
+    console.log("test passed: formatMoney function works correctly for 1000 cents.");
 }else { 
-    console.log("test failed: formatmoney function does not work correctly for 1000 cents.");
+    console.log("test failed: formatMoney function does not work correctly for 1000 cents.");
 }
 
-// edge test cases for formatmoney function
-if(formatmoney(0) === '$0.00') {
-    console.log("test passed: formatmoney function works correctly for 0 cents.");
+// edge test cases for formatMoney function
+if(formatMoney(0) === '$0.00') {
+    console.log("test passed: formatMoney function works correctly for 0 cents.");
 }
 else { 
-    console.log("test failed: formatmoney function does not work correctly for 0 cents.");
+    console.log("test failed: formatMoney function does not work correctly for 0 cents.");
 }
 
-if(formatmoney(2000.5)=== '$20.01') {
-    console.log("test passed: formatmoney function works correctly for 20005 cents.");
+if(formatMoney(2000.5)=== '$20.01') {
+    console.log("test passed: formatMoney function works correctly for 20005 cents.");
 }
 else { 
-    console.log(formatmoney(2000.5));
-    console.log("test failed: formatmoney function does not work correctly for 20005 cents.");
+    console.log(formatMoney(2000.5));
+    console.log("test failed: formatMoney function does not work correctly for 20005 cents.");
 }
 
-if(formatmoney(2000.4) === '$20.00') {
-    console.log("test passed: formatmoney function works correctly for 2000.4 cents.");
+if(formatMoney(2000.4) === '$20.00') {
+    console.log("test passed: formatMoney function works correctly for 2000.4 cents.");
 }
 else { 
-    console.log("test failed: formatmoney function does not work correctly for 2000.4 cents.");
+    console.log("test failed: formatMoney function does not work correctly for 2000.4 cents.");
 }
 
